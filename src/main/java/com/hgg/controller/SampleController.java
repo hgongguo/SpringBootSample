26059050b613d83b54702fd5e79fdfd67eaad036
hgongguo@163.com <hgg1991923>
2018-06-04 20:33:54 +0800

    1、映射端口为9090 2、配置templates静态页面反问 3、pom加打可执行jar包的plugin（spring-boot-maven-plugin）

--- src/main/java/com/hgg/controller/SampleController.java
+++ src/main/java/com/hgg/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.hgg.controller;
 
 import com.hgg.service.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
@@ -21,10 +22,24 @@ public class SampleController {
     @Autowired
     SampleService sampleService;
 
+    @Value("${weixin.appID}")
+    private String appId = "";
+
+
     @RequestMapping("/")
     @ResponseBody
     List<Map> home() {
         return sampleService.getUserInfo();
+    }
+    @RequestMapping("/appId")
+    @ResponseBody
+    String appId() {
+        return appId;
+    }
+    @RequestMapping("/hello")
+    String hello1() {
+        System.out.println("H1");
+        return "/hello";
     }
 
 }
